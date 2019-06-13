e254c120c497fa2cf96c8982c15867c1df201bd0
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-13 14:40:38 +0530

    Create fun, str, obj as a par in function.js

---
+++ fun, str, obj as a par in function.js
@@ -0,0 +1,14 @@
+// obj as a parameter in function 
+
+function someFunc(obj) {
+    console.log(obj.key1);
+    console.log(obj.key2);
+}
+  someFunc({key1: "This", key2: "works!"});
+  
+//string as a parameter in function 
+  
+  function myString(text){
+	console.log(text);
+};
+myString("Think positive")
