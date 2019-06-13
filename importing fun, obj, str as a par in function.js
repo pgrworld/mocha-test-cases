1e52af369078d673d47ab53990530f08c6cea170
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-13 16:33:46 +0530

    Update importing fun, obj, str as a par in function.js

--- importing fun, obj, str as a par in function.js
+++ importing fun, obj, str as a par in function.js
@@ -1,3 +1,7 @@
+//importing objects as a parameter in function
+var msg = require("./variables.js");
+ msg.obj({key1:"gowrav",key2 : "rahul"})
+
 // importing string a parmeter in function
 var msg = require("./variables.js");
 msg.myString("i hate pakistan");
