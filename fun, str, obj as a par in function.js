d75845d0f96548b37f3fd650efa44fc283769041
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-06-13 16:32:49 +0530

    Update fun, str, obj as a par in function.js

--- fun, str, obj as a par in function.js
+++ fun, str, obj as a par in function.js
@@ -5,6 +5,13 @@ function someFunc(obj) {
     console.log(obj.key2);
 }
   someFunc({key1: "This", key2: "works!"});
+
+                (or)
+
+exports.obj = function(obj){
+	console.log(obj.key1);
+	console.log(obj.key2);
+}
   
 //string as a parameter in function 
  exports.myString = function(text){
